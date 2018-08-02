b01f2cf9198aae61c6fb0a476f4f5b65fd9fa4a8
Gaelle <gaelle.gorgori@gmail.com>
2018-08-03 01:07:47 +0200

    ajout de routes, des helpers, du navigateur et modifation de la méthode destroy

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,7 +4,7 @@ class UsersController < ApplicationController
   end
 
   def show
-    @user = User.find(params[:id])
+   @user = User.find(params[:id])
   end
 
   def edit
@@ -31,7 +31,7 @@ class UsersController < ApplicationController
     @user.destroy
     redirect_to home_path
   end
-  
+
   private
   def user_params
     params.require(:user).permit(:username, :password_digest)
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
   root to: 'static_pages#home', as: :home
+  get 'secret', to: 'static_pages#secret', as: :secret
   get 'signup', to: 'users#new', as: :signup
   get 'login', to: 'sessions#new', as: :login
   post 'login', to: 'sessions#create'
-  post 'delete', to: 'sessions#destroy'
+  delete 'logout', to: 'sessions#destroy', as: :logout
+
   resources :users
 end
