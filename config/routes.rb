b41fd100da71f825d73b72128ac5a5c3011cfb05
tfcdg1 <fernandez.thom@gmail.com>
2018-08-02 21:20:40 +0200

    creation controllers sessions plus views sessions new plus changement routes ok

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
-
   root to: 'static_pages#home', as: :home
+  get 'signup', to: 'users#new', as: :signup
+  get 'login', to: 'sessions#new', as: :login
+  post 'login', to: 'sessions#create'
+  post 'delete', to: 'sessions#destroy'
   resources :users
-
 end
